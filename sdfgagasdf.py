fe4c9d37b6b8358a211f26048f7d767a3805b6b9
thomasakucharski <78044336+thomasakucharski@users.noreply.github.com>
2022-03-01 18:03:50 -0700

    Add files via upload

--- sdfgagasdf.py
+++ sdfgagasdf.py
@@ -18,25 +18,34 @@ except:
     print("downloaded from yahoo")
     
 
-tuple_fix = ("GOOG", "2022-01-21")
+tuple_fix = ("GOOG", "2022-01-21", "sell")
 
 
 
 
 
-def BuyStock(tuple_sample):
+def stock_change(df, tuple_sample):
     counter=0
 
     for i in df.index:
-        if i==tuple_fix[1]:
+        
+        if i==tuple_sample[1]:
             break
         else:
             counter+=1
 
-
-    zeros_list=[0 for i in range(counter)]
-    df[tuple_fix[0]][0:counter]=zeros_list
+    if tuple_sample[2] == "buy":
+        
+        zeros_list=[0 for i in range(counter)]
+        df[tuple_sample[0]][0:counter]=zeros_list
+        
+    
+    if tuple_sample[2] == "sell":
+        
+        zeros_list=[0 for i in range(len(df) - counter)]
+        df[tuple_sample[0]][counter:len(df)] = zeros_list
+        
     return df
 
-df=BuyStock(tuple_fix)
+df=stock_change(df, tuple_fix)
 
