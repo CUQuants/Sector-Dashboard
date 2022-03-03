f72814dcffebde29e5d32dbdd67631e77001b1c9
diegodalvarez <48641554+diegodalvarez@users.noreply.github.com>
2022-03-03 09:42:40 -0700

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as plt
 
 end = dt.date.today()
 start = dt.date(end.year, 1, 1)
@@ -16,36 +17,61 @@ except:
     df = yf.download(tickers, start, end)['Close']
     df.to_csv("prices.csv")
     print("downloaded from yahoo")
+
+#this function is meant for transaction data but for portfolio value (not return)
+def stock_change_port_value(df, transaction_tuple):
     
-
-tuple_fix = ("GOOG", "2022-01-21", "sell")
-
-
-
-
-
-def stock_change(df, tuple_sample):
     counter=0
 
     for i in df.index:
         
-        if i==tuple_sample[1]:
+        if i==transaction_tuple[1]:
             break
         else:
             counter+=1
+            
+    df_output = df.copy()
 
-    if tuple_sample[2] == "buy":
+    if transaction_tuple[2] == "buy":
         
         zeros_list=[0 for i in range(counter)]
-        df[tuple_sample[0]][0:counter]=zeros_list
+        df_output[transaction_tuple[0]][0:counter]=zeros_list
         
     
-    if tuple_sample[2] == "sell":
+    if transaction_tuple[2] == "sell":
         
-        zeros_list=[0 for i in range(len(df) - counter)]
-        df[tuple_sample[0]][counter:len(df)] = zeros_list
+        zeros_list=[0 for i in range(len(df_output) - counter)]
+        df_output[transaction_tuple[0]][counter:len(df_output)] = zeros_list
         
-    return df
+    return df_output
 
-df=stock_change(df, tuple_fix)
+#this function is meant for transaction data but for portfolio change (return / cumulative return) not value
+def stock_change_port_return(df, transaction_tuple):
 
+    counter = 0
+    for i in df.index:
+        
+        if i == transaction_tuple[1]:
+            break
+        
+        else:
+            counter += 1
+            
+    df_output = df.copy()
+    replace_value = df_output[transaction_tuple[0]][counter]
+            
+    if transaction_tuple[2] == "buy":
+        
+        replace_list = [replace_value for i in range(counter)]
+        df_output[transaction_tuple[0]][0:counter] = replace_list
+        
+    if transaction_tuple[2] == "sell":
+        
+        replace_list = [replace_value for i in range(len(df_output) - counter)]
+        df_output[transaction_tuple[0]][counter:len(df_output)] = replace_list
+        
+    return df_output
+
+tuple_fix = ("GOOG", "2022-01-21", "sell")
+port_value_df = stock_change_port_value(df, tuple_fix)   
+port_return_df = stock_change_port_return(df, tuple_fix)
